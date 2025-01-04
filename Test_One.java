544a5861bf44ce363133e9489b56b632363e1db9
Thangaraj-G2 <54738544+Thangaraj-G2@users.noreply.github.com>
2025-01-04 09:48:42 +0530

    First Program
    First Program for Commit

---
+++ Test_One.java
@@ -0,0 +1,9 @@
+public class Test_One
+{
+    public static void main(String[] args)
+    {
+        System.out.println("My First Jenkins Try.");
+        System.out.println("Hello World");
+
+    }
+}
